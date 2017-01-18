6036507fb90b8f3eb56a636fef1218a530d42789
Eloy Gil <eloy@eloygil.com>
2017-01-18 17:05:30 +0100

    GRASP implementation in progress, input parsing done

--- GRASP/main.py
+++ GRASP/main.py
@@ -4,9 +4,19 @@ GRASP Solver by Eloy Gil
 eloy.gil@est.fib.upc.edu
 '''
 
+# argv[1]: path to instance (ex. '../InstanceGen/instance_6.dat')
+# argv[2]: time (in seconds) to work
+
 from problem import Problem
 from solution import Solution
-import time, sys, random
+import time, sys, random, os
+
+def fileCheck(inst_path):
+    if os.path.isfile(inst_path):
+        print 'Using instance ' + inst_path.split('/')[-1]
+    else:
+        print 'File ' + inst_path + " not found. Aborting."
+        exit()
 
 def construct(alpha, problem):
     solution = Solution(problem)
@@ -23,13 +33,15 @@ def construct(alpha, problem):
         print CL, minCandidate, maxCandidate, threshold, RCL
     print "Solution: ", solution.assignments
 
+fileCheck(sys.argv[1])
 startTime = time.time()
-problem  = Problem()
-solution = Solution(problem)
-construct(0.4, problem)
+problem = Problem()
+problem.load(sys.argv[1])
+#solution = Solution(problem)
+#construct(0.4, problem)
 #print solution.isFeasible()
 print
-while time.time() - startTime < float(sys.argv[1]):
+while time.time() - startTime < float(sys.argv[2]):
     test = 1
     # test
 print "pos oc. Done."
--- GRASP/problem.py
+++ GRASP/problem.py
@@ -25,4 +25,64 @@ class Problem(object):
         self.sourcePoint = self.nPoints
         self.bigM = self.workTime * 2
 
-
+    def load(self, path):
+        self.nPoints = 0
+        self.workTime = 0
+        self.dist = []
+        self.window = []
+        self.task = []
+        self.sourcePoint = self.nPoints
+        self.bigM = self.workTime * 2
+        file = open(path, "r")
+        nPstr = "nP = "
+        workTimeStr = "workTime = "
+        taskStr = "task = ["
+        taskWindowStr = "task_window = [["
+        taskWindowLinesLeft = 0
+        distStr = "dist = ["
+        distLinesLeft = 0
+        for line in file:
+            if nPstr in line:
+                # Gets the number of destination points (nP)
+                self.nPoints = int(line.split(nPstr)[-1].split(";")[0])
+                print "nP =", self.nPoints
+            elif workTimeStr in line:
+                # Gets the maximum workTime
+                self.workTime = int(line.split(workTimeStr)[-1].split(";")[0])
+                print "workTime =", self.workTime
+            elif taskStr in line:
+                # Get the task list
+                tVal = line.split(taskStr)[-1].split(" ")
+                for i in range(1,len(tVal)-1):
+                    self.task.append(int(tVal[i]))
+                print "task =", self.task
+            elif taskWindowStr in line:
+                # Gets the windows to perform tasks in each point, 1st line
+                clean = line.split(taskWindowStr)[-1].split(']')[0].strip()
+                self.window.append([int(clean.split(' ')[0]), int(clean.split(' ')[-1])])
+                taskWindowLinesLeft = int(self.nPoints) - 1
+            elif taskWindowLinesLeft > 0:
+                # Gets the rest of lines for the window structure
+                clean = line.split('[')[-1].split(']')[0].strip()
+                self.window.append([int(clean.split(' ')[0]), int(clean.split(' ')[-1])])
+                taskWindowLinesLeft -= 1
+            elif distStr in line:
+                # Gets the distance matrix, 1st line
+                clean = line.split('[')[-1].split(']')[0].strip().split(" ")
+                dist_file = []
+                for v in clean:
+                    if v != '':
+                        dist_file.append(int(v))
+                self.dist.append(dist_file)
+                distLinesLeft = int(self.nPoints)
+            elif distLinesLeft > 0:
+                # Gets the rest of lines for the window structure
+                clean = line.split('[')[-1].split(']')[0].strip().split(" ")
+                dist_file = []
+                for v in clean:
+                    if v != '':
+                        dist_file.append(int(v))
+                self.dist.append(dist_file)
+                distLinesLeft -= 1
+        print "task_window =", self.window
+        print "dist =", self.dist
